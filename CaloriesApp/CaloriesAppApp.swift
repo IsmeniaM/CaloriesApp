721a04256bbf9a12630531c069245d6b42d77552
ismeniabrandao <brandaoismenia@gmail.com>
2023-01-08 21:18:34 +0100

    Initial Commit

---
+++ CaloriesApp/CaloriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesAppApp.swift
+//  CaloriesApp
+//
+//  Created by Ismenia Brandao on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
